b0dde66bff8d9f078495093bef8600801a2de590
patchwork-body <personal.gugfug@gmail.com>
2024-09-06 01:02:00 +0200

    refactor: move out <SignUpForm /> to external cmp

--- apps/web/src/app/[locale]/(public)/signup/page.tsx
+++ apps/web/src/app/[locale]/(public)/signup/page.tsx
@@ -1,54 +1,9 @@
-import { signup } from "@/actions/auth/signup";
-import { Button } from "@shallabuf/ui/button";
-import { Input } from "@shallabuf/ui/input";
-import { Label } from "@shallabuf/ui/label";
-import { useAction } from "next-safe-action/hooks";
+import { SignUpForm } from "@/components";
 
 export const metadata = {
   title: "Sign Up",
 };
 
 export default function Page() {
-  const { execute, isPending } = useAction(signup);
-
-  return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center">
-      <form className="flex flex-col space-y-4" action={execute}>
-        <Label htmlFor="email">Email</Label>
-        <Input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="Enter your email"
-          required
-          autoFocus={true}
-          autoComplete="email"
-        />
-
-        <Label htmlFor="email">Password</Label>
-        <Input
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Enter your password"
-          required
-          autoComplete="new-password"
-        />
-
-        <Label htmlFor="email">Confirm Password</Label>
-        <Input
-          id="confirmPassword"
-          name="confirmPassword"
-          type="password"
-          placeholder="Confirm your password"
-          required
-          autoComplete="new-password"
-        />
-
-        <Button disabled={isPending} type="submit">
-          Sign Up
-        </Button>
-      </form>
-    </div>
-  );
+  return <SignUpForm />;
 }
