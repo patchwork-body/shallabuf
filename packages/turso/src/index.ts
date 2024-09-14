902d519124655a7e15e4ac3f2e232c44e54c21c1
patchwork-body <personal.gugfug@gmail.com>
2024-09-14 22:59:11 +0200

    feat: fallback to edge runtime

--- packages/turso/src/index.ts
+++ packages/turso/src/index.ts
@@ -4,6 +4,9 @@ export let db: LibSQLDatabase<Record<string, never>>;
 
 if (process.env.NEXT_RUNTIME === "edge") {
   db = require("./turso.edge").db;
-} else {
+} else if (process.env.NODE_ENV === "development") {
   db = require("./turso.server").db;
+} else {
+  // Fallback to edge runtime
+  db = require("./turso.edge").db;
 }
