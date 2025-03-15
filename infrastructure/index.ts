c2f44cd9c15b47102dcc78f26fe76853728df476
patchwork-body <personal.gugfug@gmail.com>
2025-03-15 18:06:01 +0100

    feat: add Pulumi workflow and GKE cluster setup
    This commit introduces a new GitHub Actions workflow for Pulumi, enabling manual previews of infrastructure changes. Key changes include:
    - Created a `pull_request.yaml` workflow file for Pulumi previews.
    - Added GKE cluster setup in `index.ts` with necessary configurations.
    - Updated `package.json` to include `@pulumi/gcp` as a dependency.
    - Introduced `Pulumi.preview.yaml` for GCP project and zone configuration.
    - Refactored `Pulumi.yaml` to reflect the new template for GCP.
    - Enhanced `tsconfig.json` and `postgres.ts` for improved type handling and structure.

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,22 +1,139 @@
+import * as gcp from "@pulumi/gcp";
 import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
-import { setupPostgres } from "./components/postgres";
-import { setupNats } from "./components/nats";
-import { setupMinio } from "./components/minio";
-import { setupRedis } from "./components/redis";
 
-// Stack and environment detection
-const config = new pulumi.Config();
-const stack = pulumi.getStack();
-const isDev = stack === "dev";
+const name = "shallabuf";
 
-// Namespace
-const ns = new k8s.core.v1.Namespace("app-ns", {
-  metadata: { name: "app" },
+const engineVersion = gcp.container
+	.getEngineVersions()
+	.then((versions) => versions.latestMasterVersion);
+
+const cluster = new gcp.container.Cluster(name, {
+	initialNodeCount: 2,
+	minMasterVersion: engineVersion,
+	nodeVersion: engineVersion,
+	location: "europe-central2-a",
+	nodeConfig: {
+		machineType: "e2-small",
+		diskSizeGb: 20,
+		diskType: "pd-standard",
+		oauthScopes: [
+			"https://www.googleapis.com/auth/compute",
+			"https://www.googleapis.com/auth/devstorage.read_only",
+			"https://www.googleapis.com/auth/logging.write",
+			"https://www.googleapis.com/auth/monitoring",
+		],
+	},
 });
 
-// Components
-setupPostgres(config, ns, isDev);
-setupNats(ns, isDev);
-setupMinio(ns);
-setupRedis(ns);
+export const clusterName = cluster.name;
+
+// Manufacture a GKE-style kubeconfig. Note that this is slightly "different"
+// because of the way GKE requires gcloud to be in the picture for cluster
+// authentication (rather than using the client cert/key directly).
+export const kubeconfig = pulumi
+	.all([cluster.name, cluster.endpoint, cluster.masterAuth])
+	.apply(([name, endpoint, masterAuth]) => {
+		const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
+		return `apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: ${masterAuth.clusterCaCertificate}
+    server: https://${endpoint}
+  name: ${context}
+contexts:
+- context:
+    cluster: ${context}
+    user: ${context}
+  name: ${context}
+current-context: ${context}
+kind: Config
+preferences: {}
+users:
+- name: ${context}
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      command: gke-gcloud-auth-plugin
+      installHint: Install gke-gcloud-auth-plugin for use with kubectl by following
+        https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
+      provideClusterInfo: true
+`;
+	});
+
+const clusterProvider = new k8s.Provider(name, {
+	kubeconfig: kubeconfig,
+});
+
+export const natsNamespace = new k8s.core.v1.Namespace(
+	"nats",
+	{
+		metadata: {
+			name: "nats",
+		},
+	},
+	{ provider: clusterProvider },
+);
+
+export const natsNamespaceName = natsNamespace.metadata.apply(
+	(metadata) => metadata.name,
+);
+
+// Deploy NATS using Helm
+const nats = new k8s.helm.v3.Chart(
+	`${name}-nats`,
+	{
+		chart: "nats",
+		version: "1.2.11",
+		namespace: natsNamespaceName,
+		fetchOpts: {
+			repo: "https://nats-io.github.io/k8s/helm/charts",
+		},
+		values: {
+			nats: {
+				image: {
+					tag: "2.10.0",
+				},
+				jetstream: {
+					enabled: true,
+					memStorage: {
+						enabled: true,
+						size: "2Gi",
+					},
+					fileStorage: {
+						enabled: true,
+						size: "10Gi",
+					},
+				},
+			},
+			natsbox: {
+				enabled: true,
+			},
+		},
+	},
+	{ provider: clusterProvider },
+);
+
+new k8s.helm.v3.Release(
+	`${name}-nats-release`,
+	{
+		chart: "nats",
+		repositoryOpts: {
+			repo: "https://nats-io.github.io/k8s/helm/charts",
+		},
+		version: "1.2.11",
+		namespace: natsNamespaceName,
+		values: {
+			replicaCount: 2,
+			persistence: {
+				enabled: true,
+				size: "2Gi",
+			},
+		},
+	},
+	{ provider: clusterProvider },
+);
+
+export const natsServiceName = pulumi.interpolate`${name}-nats-client`;
+export const natsService = nats.getResource("v1/Service", "nats/nats").status;
+export const natsReleaseServiceName = pulumi.interpolate`${name}-nats-release`;
